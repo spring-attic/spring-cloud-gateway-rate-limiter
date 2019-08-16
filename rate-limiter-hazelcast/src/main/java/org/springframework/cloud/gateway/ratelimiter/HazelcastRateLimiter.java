2431cd2e01da78136f699ba874b2236f2aea956a
Alexey Nesterov <email@alexnesterov.com>
2019-08-16 13:59:20 +0100

    Initialize cluster on first request to avoid using Async
    This is another attempt to break deadlock in CF. Internal instance DNS
    record 0.name.apps.internal is only registered after application
    started. So Hazelcast can't use this domains during application startup.

--- rate-limiter-hazelcast/src/main/java/org/springframework/cloud/gateway/ratelimiter/HazelcastRateLimiter.java
+++ rate-limiter-hazelcast/src/main/java/org/springframework/cloud/gateway/ratelimiter/HazelcastRateLimiter.java
@@ -1,49 +1,79 @@
 package org.springframework.cloud.gateway.ratelimiter;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.TcpIpConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.gateway.filter.ratelimit.AbstractRateLimiter;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-import org.springframework.validation.Validator;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import org.springframework.cloud.gateway.filter.ratelimit.AbstractRateLimiter;
+import org.springframework.validation.Validator;
 
 public class HazelcastRateLimiter extends AbstractRateLimiter<HazelcastRateLimiter.RateLimiterConfig> {
 
-    static class RateLimiterConfig {
+	static class RateLimiterConfig {
 
-        private int limit;
+		private int limit;
 
-        public int getLimit() {
-            return limit;
-        }
+		public int getLimit() {
+			return limit;
+		}
 
-        public void setLimit(int limit) {
-            this.limit = limit;
-        }
-    }
+		public void setLimit(int limit) {
+			this.limit = limit;
+		}
 
-    private final Logger logger = LoggerFactory.getLogger(HazelcastRateLimiter.class);
+	}
 
-    private IMap<String, Integer> map;
-    private AtomicBoolean initialized = new AtomicBoolean(false);
+	private final Logger logger = LoggerFactory.getLogger(HazelcastRateLimiter.class);
+	private final Mono<HazelcastInstance> hazelcastCluster;
 
-    public HazelcastRateLimiter(Validator defaultValidator) {
-        super(HazelcastRateLimiter.RateLimiterConfig.class, "hazelcast-rate-limiter", defaultValidator);
-    }
+	public HazelcastRateLimiter(Validator defaultValidator, String internalHost, int instances) {
+		super(HazelcastRateLimiter.RateLimiterConfig.class, "hazelcast-rate-limiter", defaultValidator);
+		List<String> members = IntStream.range(0, instances)
+		                                .mapToObj(id -> id + "." + internalHost)
+		                                .collect(Collectors.toList());
 
-    public void initialize(List<String> members) {
-        String groupName = "my-test-group";
+		this.hazelcastCluster = Mono.defer(() -> {
+            HazelcastInstance hazelcastInstance = initCluster(internalHost, members);
+            return Mono.just(hazelcastInstance);
+		}).cache();
+	}
+
+    @Override
+	public Mono<Response> isAllowed(String routeId, String id) {
+		final Response notAllowed = new Response(false, Collections.emptyMap());
+		return this.hazelcastCluster
+				.map(hazelcastInstance -> hazelcastInstance.<String, Integer>getMap("rate-limits"))
+				.map(map -> {
+					Integer noRequests = map.getOrDefault(id, 0) + 1;
+					HazelcastRateLimiter.RateLimiterConfig config = getConfig().getOrDefault(routeId, new HazelcastRateLimiter.RateLimiterConfig());
+
+					logger.info("Current limit is {}, total requests to date {}", config.getLimit(), noRequests);
+
+					if (noRequests >= config.getLimit()) {
+						logger.info("Exceeded number of allowed requests");
+						return notAllowed;
+					}
+
+					map.put(id, noRequests);
+					final int remainingRequests = config.getLimit() - noRequests;
+
+					return new Response(true, Collections.singletonMap("X-Remaining-Limit", String.valueOf(remainingRequests)));
+				});
+	}
+
+    private HazelcastInstance initCluster(String groupName, List<String> members) {
+	    String instanceIndex = System.getenv("CF_INSTANCE_INDEX");
+	    logger.info("Starting new cluster for group {} on instance {}", groupName, instanceIndex);
 
         Config cfg = new Config();
         cfg.getGroupConfig().setName(groupName);
@@ -53,34 +83,6 @@ public class HazelcastRateLimiter extends AbstractRateLimiter<HazelcastRateLimit
         TcpIpConfig tcpIpConfig = joinConfig.getTcpIpConfig().setEnabled(true);
         members.forEach(tcpIpConfig::addMember);
 
-        HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
-        map = instance.getMap("rate-limits");
-        initialized.set(true);
-    }
-
-
-    @Override
-    public Mono<Response> isAllowed(String routeId, String id) {
-        final Response notAllowed = new Response(false, Collections.emptyMap());
-
-        if (!initialized.get()) {
-            logger.info("Cluster is not yet initialized");
-            return Mono.just(notAllowed);
-        }
-
-        Integer noRequests = map.getOrDefault(id, 0) + 1;
-        HazelcastRateLimiter.RateLimiterConfig config = getConfig().getOrDefault(routeId, new HazelcastRateLimiter.RateLimiterConfig());
-
-        logger.info("Current limit is {}, total requests to date {}", config.getLimit(), noRequests);
-
-        if (noRequests >= config.getLimit()) {
-            logger.info("Exceeded number of allowed requests");
-            return Mono.just(notAllowed);
-        }
-
-        map.put(id, noRequests);
-        final int remainingRequests = config.getLimit() - noRequests;
-
-        return Mono.just(new Response(true, Collections.singletonMap("X-Remaining-Limit", String.valueOf(remainingRequests))));
+        return Hazelcast.newHazelcastInstance(cfg);
     }
 }
--- samples/cf-hazelcast-p2p/src/main/java/sample/DemoApplication.java
+++ samples/cf-hazelcast-p2p/src/main/java/sample/DemoApplication.java
@@ -1,39 +1,23 @@
 package sample;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.ratelimiter.HazelcastRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.validation.Validator;
-
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
 public class DemoApplication {
 
-    @Autowired
-    private HazelcastRateLimiter hazelcastRateLimiter;
+	@Bean
+	public HazelcastRateLimiter hazelcastRateLimiter(Validator defaultValidator) {
+		return new HazelcastRateLimiter(defaultValidator, "peer.apps.internal", 2);
+	}
 
-    @Bean
-    public HazelcastRateLimiter hazelcastRateLimiter(Validator defaultValidator) {
-        return new HazelcastRateLimiter(defaultValidator);
-    }
-
-    // super hacky - but DNS records X.peer.apps.internal will only be available shortly after application started
-    // so InitializingBean or any other option to start cluster during app startup won't work
-    @Scheduled(initialDelay = 3000, fixedDelay = Integer.MAX_VALUE)
-    void startHazelcastCluster() {
-        List<String> members = Arrays.asList("0.peer.apps.internal", "1.peer.apps.internal");
-        hazelcastRateLimiter.initialize(members);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
 }
